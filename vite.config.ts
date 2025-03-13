adc4fa100b4d196d14ab37003574a25dd1944250
Stenmark <anthony.stenmark@sca.com.au>
2025-03-13 14:56:07 +1100

    Fixed the broken config

--- vite.config.ts
+++ vite.config.ts
@@ -7,21 +7,21 @@ export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   build: {
     chunkSizeWarningLimit: 1000,
-    rollupOptions: {
-      output: {
-        manualChunks: (id) => {
-          if (id.includes('node_modules')) {
-            if (id.includes('react') || 
-                id.includes('react-dom') || 
-                id.includes('scheduler') || 
-                id.includes('react-router') || 
-                id.includes('react-redux')) {
-              return 'react-vendor';
-            }
-            return 'vendors';
-          }
-        }
-      }
-    }
+    // rollupOptions: {
+    //   output: {
+    //     manualChunks: (id) => {
+    //       if (id.includes('node_modules')) {
+    //         if (id.includes('react') || 
+    //             id.includes('react-dom') || 
+    //             id.includes('scheduler') || 
+    //             id.includes('react-router') || 
+    //             id.includes('react-redux')) {
+    //           return 'react-vendor';
+    //         }
+    //         return 'vendors';
+    //       }
+    //     }
+    //   }
+    // }
   }
 })
