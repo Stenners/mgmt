bc2ccd5ff8da2e03ed04b6a7e33f70bf80531328
Stenmark <anthony.stenmark@sca.com.au>
2025-04-11 10:05:15 +1000

    MGMT Upate

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -29,7 +29,7 @@ export const Navbar = () => {
             href="/"
           >
             <Logo />
-            <p className="font-bold text-inherit">ACME</p>
+            <p className="font-bold text-inherit">MGMT</p>
           </Link>
         </NavbarBrand>
         <div className="hidden lg:flex gap-4 justify-start ml-2">
